2f7f815af422d871931d8d8be6c1611fc82ac883
Aksel Pleym <akselbyt@gmail.com>
2020-12-14 10:58:38 +0100

    julebanner-uke51

--- Aksel/uke51/julebanner/script.js
+++ Aksel/uke51/julebanner/script.js
@@ -1,6 +1,7 @@
 var bilder = [ "<img src='bilder/1.jpg'>", "<img src='bilder/2.jpg'>", "<img src='bilder/3.jpg'>" ]
-const btnVis = document.querySelector("#btnVis")
-const btnTilbake = document.querySelector("#btnTilbake")
+var tekst = ["", "<p id='del1'>blablablabla 2</p>", "<p id='del2'>Håper du for en god JUL<br> og et godt NYTTÅR</p>"]
+const btnMusic = document.querySelector("#btnMusic")
+const audio = document.querySelector("audio");
 
 var i = 0
 window.onload = oppstart;
@@ -10,9 +11,7 @@ function oppstart() {
 }
 var test = document.getElementById("txtDiv");
 
-
-
-txtDiv.innerHTML = bilder[0]
+txtDiv.innerHTML = bilder[0] + tekst[0]
 setInterval(neste, 3000);
 function neste() {
     if(i == bilder.length -1) {
@@ -22,7 +21,19 @@ function neste() {
             i++
     }
     test.className += "fade-in"
-    txtDiv.innerHTML = bilder[i]
+    txtDiv.innerHTML = bilder[i] + tekst[i]
 }
+
+//Musikk 
+btnMusic.onclick = () => {
+    if (audio.paused) {
+      audio.volume = 0.2;
+      audio.play();    
+    } else {
+      audio.pause();
+    }
+}
+
+ 
 
 
